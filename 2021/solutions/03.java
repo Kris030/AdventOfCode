4766dc4e8606d96d0dae2bc49acfd7a99ede4ccc
Kristóf Bácskai <45177246+Kris030@users.noreply.github.com>
2021-12-03 20:55:47 +0100

    Finish day 3 part 1 solution

--- 2021/solutions/03.java
+++ 2021/solutions/03.java
@@ -5,35 +5,30 @@ class Day03 {
 	public static void main(String[] args) {
 		partOne();
 	}
-	
+
 	public static void partOne() {
 		try (Scanner sc = new Scanner(System.in)) {
 			String s = sc.next();
-			int bs = s.lenght();
+			int bs = s.length();
 			int[][] bc = new int[bs][2];
-			
-			do {
+
+			for (int i = 0; i < bs; i++)
+				bc[i][(int) (s.charAt(i) - '0')]++
+
+			while (sc.hasNext()) {
+				s = sc.next();
 				for (int i = 0; i < bs; i++)
 					bc[i][(int) (s.charAt(i) - '0')]++;
-				s = sc.next();
-			} while (sc.hasNext());
-
-			int gamma = 0, epsilon = 0;
-			for (int i = 0; i < bs; i++) {
-				int maxi, mini;
-				if (bc[i][0] > bc[i][1]) {
-					maxi = 0;
-					mini = 1;
-				} else {
-					maxi = 1;
-					mini = 0;
-				}
-				gamma   |= maxi <<< i;
-				epsilon |= mini <<< i;
 			}
 
-			System.out.println(gamma * epsilon);
+			int g = 0, e = 0;
+			for (int i = 0; i < bs; i++) 
+				if (bc[i][0] > bc[i][1])
+					e |= 1 << (bs - i - 1);
+				else
+					g |= 1 << (bs - i - 1);
+
+			System.out.println(g * e);
 		}
 	}
-
 }
