a4848d8ca929b3b28ebf5590725465c1c60954b3
the boi <45177246+Kris030@users.noreply.github.com>
2021-12-03 13:06:51 +0100

    asd

--- 2021/solutions/03.java
+++ 2021/solutions/03.java
@@ -8,15 +8,18 @@ class Day03 {
 	
 	public static void partOne() {
 		try (Scanner sc = new Scanner(System.in)) {
-			int[][] bc = new int[5][2];
+			String s = sc.next();
+			int bs = s.lenght();
+			int[][] bc = new int[bs][2];
 			
-			while (sc.hasNext()) {
-				String s = sc.next();
-				for (int i = 0; i < 5; i++)
+			do {
+				for (int i = 0; i < bs; i++)
 					bc[i][(int) (s.charAt(i) - '0')]++;
-			}
+				s = sc.next();
+			} while (sc.hasNext());
+
 			int gamma = 0, epsilon = 0;
-			for (int i = 0; i < 5; i++) {
+			for (int i = 0; i < bs; i++) {
 				int maxi, mini;
 				if (bc[i][0] > bc[i][1]) {
 					maxi = 0;
